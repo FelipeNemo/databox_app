07960dcdefbf709a9433fbc2fee2e20f93a22a98
Felipe <felipedata20@gmail.com>
2025-07-22 15:20:55 -0300

    add: cadastro usuário via frontend

--- backend/userauths/urls.py
+++ backend/userauths/urls.py
@@ -1,17 +1,20 @@
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse
+from .views import RegisterView, LoginView
 
 def home_view(request):
     return JsonResponse({"mensagem": "Backend funcionando 🚀"})
 
+
 urlpatterns = [
-    path('', home_view, name='home'),
-    path("admin/", admin.site.urls),
-    path("auth/", include("userauths.urls")),  # <- ISSO É ESSENCIAL
+    path("estudante/register/", RegisterView.as_view(), name="register_estudante"),
+    path("empresa/register/", RegisterView.as_view(), name="register_empresa"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
 
+# ✅ Essas linhas servem para exibir imagens, CSS etc durante o desenvolvimento
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
